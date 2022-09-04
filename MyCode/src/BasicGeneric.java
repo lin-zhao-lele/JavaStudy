ecae04b16717d5359a4cb712d6b5a08a7beaad11
lzhao <c-lzhao@powercosts.com>
2022-09-04 14:03:41 -0500

    second commit

--- MyCode/src/BasicGeneric.java
+++ MyCode/src/BasicGeneric.java
@@ -7,6 +7,9 @@ public class BasicGeneric {
     public static void main(String[] args) {
         // TODO Auto-generated method stub
 
+
+        //
+        System.out.println("Done! *********************************************** ");
     }
 
 
