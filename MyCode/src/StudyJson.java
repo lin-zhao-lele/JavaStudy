a07b3bf032627e95efb9678aeca715cea45d4e47
lzhao <lzhao@pcienergysolutions.com>
2023-03-21 18:27:50 -0400

    From new mac. Add Json object Gen 2

--- MyCode/src/StudyJson.java
+++ MyCode/src/StudyJson.java
@@ -1,3 +1,5 @@
+import lombok.Builder;
+import lombok.Data;
 import lombok.NonNull;
 import lombok.Value;
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -6,10 +8,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class StudyJson {
 
-    //要将一个Java对象转换成字符串，通常需要使用序列化（serialization）或者JSON格式化。
-    public static void main(String... args) throws JsonProcessingException {
-        RequestBody requestBody = new RequestBody("password");
 
+    public static void main(String... args) throws JsonProcessingException {
+        RequestBody requestBody = RequestBody.builder().grantType("Type").grantID("ID").build();;
+
+        // 要将一个Java对象转换成字符串，通常需要使用序列化（serialization）或者JSON格式化。
         ObjectMapper objectMapper = new ObjectMapper();
         String requestBodyJson = objectMapper.writeValueAsString(requestBody);
 
@@ -17,11 +20,16 @@ public class StudyJson {
     }
 
 
+    @Data
+    @Builder
     @Value
     private static class  RequestBody
     {
         @NonNull
         @JsonProperty("grant_type")
         private String grantType;
+        @NonNull
+        @JsonProperty("grant_id")
+        private String grantID;
     }
 }
